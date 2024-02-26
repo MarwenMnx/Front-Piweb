96deaae95883ffedd788bc460199462450cdb54f
Marwen-Neji <78695522+Marwen-Neji@users.noreply.github.com>
2024-02-26 16:44:48 +0100

    auth front end implementation

--- src/components/auth/auth.jsx
+++ src/components/auth/auth.jsx
@@ -1,89 +1,98 @@
+import React, { useState } from "react";
+import { useDispatch } from 'react-redux'; 
+import "./auth.css";
+import * as api from '../../api/userApi';
+import { register, login } from '../../actions/authActions'; 
 
-import React, { useState } from "react"
-import "./auth.css"
+export default function Auth(props) {
+  const dispatch = useDispatch(); 
+  const [authMode, setAuthMode] = useState("signin");
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [role, setRole] = useState('');
+  const rolesEnum = ['Administrator', 'Energy Manager', 'Operator'];
 
-export default function (props) {
-  let [authMode, setAuthMode] = useState("signin")
+  const handleSubmit = async (event) => {
+    event.preventDefault(); 
+    try {
+      if (authMode === "signup") {
+        
+        await dispatch(register({ username, password, role }));
+        console.log("User created successfully");
+      } else {
+       
+        await dispatch(login({ username, password }));
+        console.log("user signed in");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
 
   const changeAuthMode = () => {
-    setAuthMode(authMode === "signin" ? "signup" : "signin")
-  }
-
-  if (authMode === "signin") {
-    return (
-      <div className="Auth-form-container">
-        <form className="Auth-form">
-          <div className="Auth-form-content">
-            <h3 className="Auth-form-title">Sign In</h3>
-            <div className="text-center">
-              Not registered yet?{" "}
-              <span className="link-primary" onClick={changeAuthMode}>
-                Sign Up
-              </span>
-            </div>
-            <div className="form-group mt-3">
-              <label>Email address</label>
-              <input
-                type="email"
-                className="form-control mt-1"
-                placeholder="Enter email"
-              />
-            </div>
-            <div className="form-group mt-3">
-              <label>Password</label>
-              <input
-                type="password"
-                className="form-control mt-1"
-                placeholder="Enter password"
-              />
-            </div>
-            <div className="d-grid gap-2 mt-3">
-              <button type="submit" className="btn btn-primary">
-                Submit
-              </button>
-            </div>
-            <p className="text-center mt-2">
-               <a href="#">Forgot password?</a>
-            </p>
-          </div>
-        </form>
-      </div>
-    )
-  }
+    setAuthMode(authMode === "signin" ? "signup" : "signin");
+  };
 
   return (
     <div className="Auth-form-container">
-      <form className="Auth-form">
+      <form className="Auth-form" onSubmit={handleSubmit}>
         <div className="Auth-form-content">
-          <h3 className="Auth-form-title">Sign In</h3>
+          <h3 className="Auth-form-title">{authMode === "signin" ? "Sign In" : "Sign Up"}</h3>
           <div className="text-center">
-            Already registered?{" "}
+            {authMode === "signin" ? "Not registered yet? " : "Already registered? "}
             <span className="link-primary" onClick={changeAuthMode}>
-              Sign In
+              {authMode === "signin" ? "Sign Up" : "Sign In"}
             </span>
           </div>
-          <div className="form-group mt-3">
-            <label>Full Name</label>
-            <input
-              type="email"
-              className="form-control mt-1"
-              placeholder="Enter your Name"
-            />
-          </div>
-          <div className="form-group mt-3">
-            <label>Email address</label>
-            <input
-              type="email"
-              className="form-control mt-1"
-              placeholder="Enter Email Address"
-            />
-          </div>
+          {authMode === "signup" && (
+            <div>
+              <div className="form-group mt-3">
+                <label>username</label>
+                <input
+                  type="text"
+                  className="form-control mt-1"
+                  placeholder="Enter your username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </div>
+              <div className="form-group mt-3">
+                <label>User Role</label>
+                <select
+                  className="form-select"
+                  value={role}
+                  onChange={(e) => setRole(e.target.value)}
+                >
+                  <option value="">Select User Role</option>
+                  {rolesEnum.map((role, index) => (
+                    <option key={index} value={role}>
+                      {role}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+          )}
+          {authMode === "signin" && (
+            <div className="form-group mt-3">
+              <label>username</label>
+              <input
+                type="text"
+                className="form-control mt-1"
+                placeholder="Enter your username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+          )}
           <div className="form-group mt-3">
             <label>Password</label>
             <input
               type="password"
               className="form-control mt-1"
               placeholder="Enter Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <div className="d-grid gap-2 mt-3">
@@ -97,5 +106,5 @@ export default function (props) {
         </div>
       </form>
     </div>
-  )
+  );
 }

0c9f9c40e9867fa95251a8f9a0945731826e77d4
SirineZoghlami <sirine.zoghlami@esprit.tn>
2024-02-27 23:05:32 +0100

    template

---
+++ src/pages/Settings.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Col, Row, Button, Dropdown } from '@themesberg/react-bootstrap';
+import { ChoosePhotoWidget, ProfileCardWidget } from "../components/Widgets";
+import { GeneralInfoForm } from "../components/Forms";
+
+import Profile3 from "../assets/img/team/profile-picture-3.jpg";
+
+
+export default () => {
+  return (
+    <>
+      <Row>
+        <Col xs={12} xl={8}>
+          <GeneralInfoForm />
+        </Col>
+
+        <Col xs={12} xl={4}>
+          <Row>
+            <Col xs={12}>
+              <ChoosePhotoWidget
+                title="Select profile photo"
+                photo={Profile3}
+              />
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </>
+  );
+};
---
+++ src/routes.jsx
@@ -0,0 +1,11 @@
+
+export const Routes = {
+    // pages
+    Signin: { path: "/" },
+    DashboardOverview: { path: "/dashboard/overview" },
+    Settings: { path: "/settings" },
+    Signup: { path: "/sign-up" },
+    ForgotPassword: { path: "/forgot-password" },
+    ResetPassword: { path: "/reset-password" },
+    NotFound: { path: "/examples/404" },
+};
